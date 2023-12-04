eb263210c60531cd56e55689e0cf41f763adebd5
nivedin <nivedinp@gmail.com>
2023-12-04 14:15:46 +0530

    chore: update graphql with ts and setup

--- packages/hoppscotch-common/src/components/graphql/Headers.vue
+++ packages/hoppscotch-common/src/components/graphql/Headers.vue
@@ -269,6 +269,7 @@ import { useCodemirror } from "@composables/codemirror"
 import { objRemoveKey } from "~/helpers/functional/object"
 import { useVModel } from "@vueuse/core"
 import { HoppGQLHeader } from "~/helpers/graphql"
+import { throwError } from "~/helpers/functional/error"
 
 const colorMode = useColorMode()
 const t = useI18n()
@@ -384,7 +385,6 @@ watch(workingHeaders, (newWorkingHeaders) => {
     )
   )
 
-  console.log("not-equal", workingHeaders.value)
   if (!isEqual(request.value.headers, fixedHeaders)) {
     request.value.headers = cloneDeep(fixedHeaders)
   }
@@ -477,7 +477,11 @@ const deleteHeader = (index: number) => {
     })
   }
 
-  workingHeaders.value.splice(index, 1)
+  workingHeaders.value = pipe(
+    workingHeaders.value,
+    A.deleteAt(index),
+    O.getOrElseW(() => throwError("Working Headers Deletion Out of Bounds"))
+  )
 }
 
 const clearContent = () => {
@@ -575,6 +579,4 @@ const mask = (header: any) => {
 //   if (tab === "auth") emit("change-tab", "authorization")
 //   else emit("change-tab", "bodyParams")
 // }
-
-console.log("compoyed-header", computedHeaders.value)
 </script>
